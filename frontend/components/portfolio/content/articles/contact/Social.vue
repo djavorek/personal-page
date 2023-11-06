2023098473f735160cb263dab9b63fbdf74aa9ec
Javorek Dénes <javorek.denes@gmail.com>
2023-11-06 08:35:52 +0100

    fix: propert fix for social icon sizes

--- frontend/components/portfolio/content/articles/contact/Social.vue
+++ frontend/components/portfolio/content/articles/contact/Social.vue
@@ -11,7 +11,7 @@
         target="_blank"
         rel="noopener noreferrer"
       >
-        <font-awesome-icon icon="fa-brands fa-linkedin" size="xl" />
+        <font-awesome-icon class="socialicon" icon="fa-brands fa-linkedin" />
         <span>/in/denes-javorek/</span>
       </a>
     </div>
@@ -21,28 +21,16 @@
         target="_blank"
         rel="noopener noreferrer"
       >
-        <font-awesome-icon icon="fa-brands fa-facebook" size="xl" />
+        <font-awesome-icon class="socialicon" icon="fa-brands fa-facebook" />
         <span>facebook.com/javidenes/</span>
       </a>
     </div>
     <div class="contact">
-      <font-awesome-icon icon="fa-brands fa-discord" size="xl" />
+      <font-awesome-icon class="socialicon" icon="fa-brands fa-discord" />
       <span>Dénes#2583</span>
     </div>
   </div>
 </template>
-
-<script>
-export default {
-  data() {
-    return {
-      mailImg: require('@/assets/img/contact_mail.svg'),
-      inImg: require('@/assets/img/contact_in.svg'),
-      fbImg: require('@/assets/img/contact_fb.svg'),
-    };
-  },
-};
-</script>
 
 <style scoped lang="scss">
 @use '~/assets/style/common';
@@ -56,8 +44,8 @@ export default {
   padding-top: 2em;
 }
 
-img {
-  width: 2.5em;
+.socialicon {
+  width: 2em;
   vertical-align: middle;
 }
 
